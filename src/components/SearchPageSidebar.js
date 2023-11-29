4a8238cd013caabfa7a4dd134724dc4d260ba945
farhan-sadik247 <fsadik2319@gmail.com>
2023-11-29 18:23:59 +0600

    commit done

--- src/components/SearchPageSidebar.js
+++ src/components/SearchPageSidebar.js
@@ -1,4 +1,25 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
 function SearchPageSidebar() {
+    const [title, settitle] = useState("")
+    const [description, setdescription] = useState("")
+    const [subscriptionAmount, setSubscriptionAmount] = useState("")
+    const [techs, settechs] = useState("")
+    const [catas, setCatas] = useState([])
+    const [cata, setCata] = useState("")
+    const [warning, usercheck] = useState(false)
+    const goHome = useNavigate()
+    const getCatas = async() => {
+        let res = await fetch("course/getcata")
+        let data = await res.json()
+        setCatas(data)
+    }
+    useEffect(
+        () => {getCatas()},[]
+    )
     return (
         <div className="container mb-5">
         <div className="card mt-5">
@@ -16,30 +37,17 @@ function SearchPageSidebar() {
                 </div>
             </div>
 
-            <div className="mb-3">
-                <label className="px-2"><h5>Category</h5></label>
-                <div>
-                <label className="px-2">
-                    <input type="checkbox" value="python" /> Python
-                </label>
-                <br />
-                <label className="px-2">
-                    <input type="checkbox" value="java" /> Java
-                </label>
-                <br />
-                <label className="px-2">
-                    <input type="checkbox" value="laravel" /> Laravel
-                </label>
-                <br />
-                <label className="px-2">
-                    <input type="checkbox" value="css" /> CSS
-                </label>
-                <br />
-                <label className="px-2">
-                    <input type="checkbox" value="cpp" /> C++
-                </label>
-                </div>
+            {/* Category */}
+            <div className="mt-1 ">
+            <label htmlFor="title" className="form-label px-2"><h5>Category</h5></label> 
+            <select name="category" className="form-control"  onChange={(e)=> {setCata(e.target.value)}}>
+                <option value="" ></option>
+                {catas.map((name, index) =>
+                    <option value={catas[index].id} >{catas[index].title}</option> )}
+                
+                </select>                         
             </div>
+            {/* Category end */}
             </div>
         </div>
         </div>
